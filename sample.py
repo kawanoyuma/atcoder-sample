44f308323dc38d41955af0b88d08840ca2ff07ce
YumaKawano <yuma.kawano.biz@gmail.com>
2020-06-07 12:14:36 +0900

    AtCoder-116-A

--- sample.py
+++ sample.py
@@ -1,34 +1,9 @@
-#166-B（Trick or Treat）
-
-#Input
-Inputs = []
+#116-A（Right Triangle）
 Input = input().split()
-
-N = Input[0]
-N = int(N)
-Person = []
-for l in range(N):
-    Person.append(l+1)
-
-K = Input[1]
-K = int(K)
-K2 = K * 2
-Inputs.append(Input)
-
-for l in range(K2):
-    Input = input().split()
-    Inputs.append(Input)
-
-#Check
-for a in range(K):
-    A = (a + 1)*2 
-    L = Inputs[A - 1][0]
-    L = int(L)
-    for b in range(L):
-        P = Inputs[A][b]
-        P = int(P)
-        if P in Person:
-            Person.remove(P)
-
-#Output
-print(len(Person))
+AB = Input[0]
+AB = int(AB)
+BC = Input[1]
+BC = int(BC)
+Answer = AB * BC / 2
+Answer = int(Answer)
+print(Answer)
