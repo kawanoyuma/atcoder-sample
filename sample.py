1352d6259d7b8c94808285badab784019333bd68
YumaKawano <yuma.kawano.biz@gmail.com>
2020-06-07 12:49:49 +0900

    AtCoder-116-B

--- sample.py
+++ sample.py
@@ -1,9 +1,19 @@
-#116-A（Right Triangle）
-Input = input().split()
-AB = Input[0]
-AB = int(AB)
-BC = Input[1]
-BC = int(BC)
-Answer = AB * BC / 2
-Answer = int(Answer)
-print(Answer)
+#116-B（Collatz Problem）
+a = []
+s = input()
+s = int(s)
+a.append(s)
+
+i = 0
+while i <= 1000000:
+    n = a[-1]
+    n = int(n)
+    if n % 2 == 0:
+        C = n / 2
+    else:
+        C = n * 3 + 1
+    if C in a:
+        a.append(C)
+        break
+    a.append(C)
+print(len(a))
