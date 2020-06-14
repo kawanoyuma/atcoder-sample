e3ae8e6695c89f11f6ff8c44a9c5ae4a3f67f534
YumaKawano <yuma.kawano.biz@gmail.com>
2020-06-14 16:22:43 +0900

    AtCoder-169-D

--- sample.py
+++ sample.py
@@ -1,25 +1,50 @@
-#169-B（Multiplication2）
+#169-D (Div Game)
 
+#入力
 N = input()
 N = int(N)
-A = input().split()
 
-minA = min(A)
-minA = int(minA)
-M = 10 ** 18
+#素因数分解
+Num = []
+n = N
+for i in range(2, int(-(-N**0.5//1))+1):
+    if n%i==0:
+        j=0
+        while n%i==0:
+            j+=1
+            n //= i
+        Num.append(j)
 
-Y = 0
-if minA != 0:
-    Y = 1
-    for i in range(N):
-        X = A[i]
-        X = int(X)
-        Y = Y * X
-        if Y > M:
-            break
+if n!=1:
+    Num.append(1)
 
+if Num==[]:
+    if N == 1:
+        Num.append(0)
+    else:
+        NUm.append(1)
 
-if Y > M:
-    print(-1)
-else:
-    print(Y)
+#階差数列
+P = []
+p = 0
+l = max(Num)
+for i in range(1,l+1):
+    p += i
+    P.append(p)
+
+#計算
+Answer = 0
+
+l = len(Num)
+
+if Num[0] != 0:
+    for i in range(l):
+        q = Num[i]
+        while not(q in P):
+            q -= 1
+        Index = P.index(q)
+        Index = int(Index)
+        Index += 1
+        Answer += Index
+
+print(Answer)
