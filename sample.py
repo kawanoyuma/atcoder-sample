adff56fc3309da1ba49d115c2a42461139f0a06e
YumaKawano <yuma.kawano.biz@gmail.com>
2020-06-14 12:55:09 +0900

    AtCoder-169-B

--- sample.py
+++ sample.py
@@ -1,8 +1,25 @@
-#169-A（Multiplication1）
+#169-B（Multiplication2）
 
-Input = input().split()
-A = int(Input[0])
-B = int(Input[1])
-AB = A * B
+N = input()
+N = int(N)
+A = input().split()
 
-print(AB)
+minA = min(A)
+minA = int(minA)
+M = 10 ** 18
+
+Y = 0
+if minA != 0:
+    Y = 1
+    for i in range(N):
+        X = A[i]
+        X = int(X)
+        Y = Y * X
+        if Y > M:
+            break
+
+
+if Y > M:
+    print(-1)
+else:
+    print(Y)
