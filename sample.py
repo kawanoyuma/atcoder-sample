4228776fb47a8595c895ee68fd6a76a38eb9ca1f
YumaKawano <yuma.kawano.biz@gmail.com>
2020-06-21 12:36:52 +0900

    AtCoder-170-B

--- sample.py
+++ sample.py
@@ -1,13 +1,22 @@
-#170-A (Five Variables)
+#170-B (Crane and Turtle)
 
 Input = input().split()
+X = Input[0]
+X = int(X)
+Y = Input[1]
+Y = int(Y)
 
-X = 0
-for i in range(5):
-    M = Input[i]
-    M = int(M)
-    N = i + 1
-    if M != N:
-        X = N
+A = 0
+for i in range(X+1):
+    C = i
+    T = X - C
+    CL = C * 2
+    TL = T * 4
+    L = CL + TL
+    if L == Y:
+        A += 1
 
-print(X)
+if A == 0:
+    print("No")
+else:
+    print("Yes")
