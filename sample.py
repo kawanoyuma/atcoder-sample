801e740f0a597163772b0e69528446909867bbe5
YumaKawano <yuma.kawano.biz@gmail.com>
2020-06-21 14:54:00 +0900

    AtCoder-170-C

--- sample.py
+++ sample.py
@@ -1,22 +1,29 @@
-#170-B (Crane and Turtle)
+#170-C (Forbidden List)
 
 Input = input().split()
 X = Input[0]
 X = int(X)
-Y = Input[1]
-Y = int(Y)
-
-A = 0
-for i in range(X+1):
-    C = i
-    T = X - C
-    CL = C * 2
-    TL = T * 4
-    L = CL + TL
-    if L == Y:
+N = Input[1]
+N = int(N)
+if N != 0:
+    P = input().split()
+    Z = 0
+    A = 0
+    while Z < 1:
+        B = X + A
+        C = X - A
+        B = str(B)
+        C = str(C)
+        if not(C in P):
+            D = C
+            Z = 1
+            break
+        if not(B in P):
+            D = B
+            Z = 1
+            break
         A += 1
-
-if A == 0:
-    print("No")
 else:
-    print("Yes")
+    D = X
+
+print(D)
