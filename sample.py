195fc1579c034992bc8531d24841bf3b189d8139
YumaKawano <yuma.kawano.biz@gmail.com>
2020-12-06 12:29:05 +0900

    AtCoder-184-B

--- sample.py
+++ sample.py
@@ -1,29 +1,14 @@
-#170-C (Forbidden List)
+#184-B (Quizzes)
 
-Input = input().split()
-X = Input[0]
-X = int(X)
-N = Input[1]
-N = int(N)
-if N != 0:
-    P = input().split()
-    Z = 0
-    A = 0
-    while Z < 1:
-        B = X + A
-        C = X - A
-        B = str(B)
-        C = str(C)
-        if not(C in P):
-            D = C
-            Z = 1
-            break
-        if not(B in P):
-            D = B
-            Z = 1
-            break
-        A += 1
-else:
-    D = X
-
-print(D)
+NX = input().split()
+S = list(input())
+N = int(NX[0])
+X = int(NX[1])
+P = X
+for i in range(N):
+    if S[i] == "o":
+        P += 1
+    else:
+        if P!=0:
+            P -= 1
+print(P)
